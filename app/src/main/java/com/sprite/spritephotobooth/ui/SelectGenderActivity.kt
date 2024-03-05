a341f6866cd75925ed31468dac2cab98e92b5af7
Amit <amit@webcontxt.in>
2024-03-05 17:33:55 +0530

    push

--- app/src/main/java/com/sprite/spritephotobooth/ui/SelectGenderActivity.kt
+++ app/src/main/java/com/sprite/spritephotobooth/ui/SelectGenderActivity.kt
@@ -2,14 +2,12 @@ package com.sprite.spritephotobooth.ui
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.sprite.spritephotobooth.Base.BaseActivity
-import com.sprite.spritephotobooth.R
-import com.sprite.spritephotobooth.databinding.ActivityMainBinding
-import com.sprite.spritephotobooth.databinding.ActivitySelectGenderBinding
 import android.view.WindowManager
+import com.sprite.spritephotobooth.Base.BaseActivity
+import com.sprite.spritephotobooth.databinding.ActivitySelectGenderBinding
+
+
 class SelectGenderActivity : BaseActivity() {
     private var binding: ActivitySelectGenderBinding? = null
     companion object{
@@ -44,16 +42,30 @@ class SelectGenderActivity : BaseActivity() {
 
     override fun setupListener() {
         binding?.maleButton?.setOnClickListener {
-            val intent = Intent(this@SelectGenderActivity, SelectSceneActivity::class.java)
+
+            val prefrence = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE)
+            val intent = Intent(this@SelectGenderActivity, StrikePoseActivity::class.java)
+
             intent.putExtra("gender", "male")
+            intent.putExtra("template", prefrence.getInt("template",0))
             startActivity(intent)
+
+            val editor = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE).edit()
+            editor.putInt("mtemplate", if(prefrence.getInt("mtemplate",0)==7) 0 else prefrence.getInt("mtemplate",0)+1)
+            editor.apply()
 
         }
 
         binding?.femaleButton?.setOnClickListener {
-            val intent = Intent(this@SelectGenderActivity, SelectSceneActivity::class.java)
+            val prefrence = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE)
+            val intent = Intent(this@SelectGenderActivity, StrikePoseActivity::class.java)
             intent.putExtra("gender", "female")
+            intent.putExtra("template", prefrence.getInt("template",0))
             startActivity(intent)
+
+            val editor = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE).edit()
+            editor.putInt("ftemplate", if(prefrence.getInt("ftemplate",0)==6) 0 else prefrence.getInt("ftemplate",0)+1)
+            editor.apply()
 
 
         }
